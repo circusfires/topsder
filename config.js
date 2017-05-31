a4e2374876ea1dd85822d7a0fe98f608d7349273
Asare L. Cobblah <cobblaha@gmail.com>
2017-05-31 02:51:51 -0500

    Update config.

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.clientId='WXfm6_09bm8tPg';
+config.clientSecret=process.env.RED_SEC_KEY;
+config.username=process.env.RED_USR;
+config.password=process.env.RED_PSWD;
+config.spot_secretId=process.env.SP_SEC_KEY;
+module.exports = config;
